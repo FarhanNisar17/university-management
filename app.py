c95b783ec83e1f74c67fe0f1ec0241c91ea9bfa6
Shah Arsalan <shaharsalan1901@gmail.com>
2025-11-11 13:05:23 +0530

    app.py

--- app.py
+++ app.py
@@ -14,6 +14,20 @@ def home():
 def students():
     return render_template('students.html')
 
+#scholarships
+@app.route('/scholarships')
+def scholarships():
+    return render_template('scholarships.html')
+
+#courses
+@app.route('/courses')
+def courses():
+    return render_template('courses.html')
+
+#contacts
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
 
 # Run the app
 if __name__ == '__main__':
