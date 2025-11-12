34a3c63d878324733ba7f76f61635ad1f0a819b0
Shah Arsalan <shaharsalan1901@gmail.com>
2025-11-12 10:57:34 +0530

    testing

--- app.py
+++ app.py
@@ -11,23 +11,38 @@ def home():
 
 # Students
 @app.route('/register')
-def students():
+def register():
     return render_template('views/register.html')
 
 #scholarships
 @app.route('/scholarships')
 def scholarships():
-    return render_template('scholarships.html')
+    return render_template('views/scholarships.html')
+
+#departments
+@app.route('/departments')
+def departments():
+    return render_template('views/departments.html')
+
+#hostels
+@app.route('/hostels')
+def hostels():
+    return render_template('views/hostels.html')
+
+#transport
+@app.route('/transport')
+def transport():
+    return render_template('views/transport.html')
 
 #courses
 @app.route('/courses')
 def courses():
-    return render_template('courses.html')
+    return render_template('views/courses.html')
 
-#contacts
-@app.route('/contacts')
-def contacts():
-    return render_template('contacts.html')
+#contact
+@app.route('/contact')
+def contact():
+    return render_template('views/contact.html')
 
 # Run the app
 if __name__ == '__main__':
