7a9840fe387f1f0e126a09d2c6d9d5600fbdbbbf
Shah Arsalan <shaharsalan1901@gmail.com>
2025-11-18 10:48:02 +0530

    a

--- util/config.py
+++ util/config.py
@@ -1,4 +1,4 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:urdream@localhost/university_db"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:shah1901@localhost/university_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "your-secret-key"
